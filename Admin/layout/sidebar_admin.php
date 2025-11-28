2e25b027080e6f4fbaec4ee160ea4e75cd85c178
EuphoriaBot <dimasfirja80@gmail.com>
2025-11-28 14:33:42 +0800

    Add little detail

--- Admin/layout/sidebar_admin.php
+++ Admin/layout/sidebar_admin.php
@@ -1,11 +1,15 @@
+<?php 
+$current_page = basename($_SERVER['PHP_SELF']);
+?>
+
 <section class="sidebar">
   <ul class="sidebar-menu">
-    <li class="manage-menu active">
+    <li class="manage-menu <?php if ($current_page == 'manage_menu.php') {echo 'active';} ?>">
       <a href="../menu/manage_menu.php">Manage Menu</a>
     </li>
-    <li class="manage-review">
+    <li class="manage-review <?php if ($current_page == 'manage_reviews.php') {echo 'active';} ?>">
       <a href="../ulasan/manage_reviews.php">Manage Reviews</a>
     </li>
-    <li class="manage berita"><a href="../berita/manage_berita.php">Manage Berita</a></li>
+    <li class="manage berita <?php if ($current_page == 'manage_berita.php') {echo 'active';} ?>"><a href="../berita/manage_berita.php">Manage Berita</a></li>
   </ul>
 </section>
