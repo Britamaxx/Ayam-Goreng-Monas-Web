49aa0f22298b97638c14038ac4cd6de4cb87154d
404-DevNotFound <dante.engineer@gmail.com>
2025-11-28 23:49:47 +0800

    typo

--- Admin/layout/sidebar_admin.php
+++ Admin/layout/sidebar_admin.php
@@ -5,7 +5,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
 <section class="sidebar">
   <ul class="sidebar-menu">
     <li class="dashboard <?php if ($current_page == 'dashboard_admin.php') {echo 'active';} ?>">
-      <a href="../dashboard/dashboard_admin.php">dashboard</a>
+      <a href="../dashboard/dashboard_admin.php">Dashboard</a>
     </li>
     <li class="manage-menu <?php if ($current_page == 'manage_menu.php') {echo 'active';} ?>">
       <a href="../menu/manage_menu.php">Manage Menu</a>
