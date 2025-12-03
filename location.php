01e9564290066f14d9f7f04970a8988638311f22
404-DevNotFound <dante.engineer@gmail.com>
2025-12-03 23:04:40 +0800

    pisahin footer

--- location.php
+++ location.php
@@ -78,56 +78,7 @@ $lokasi = mysqli_query($conn, "SELECT * FROM lokasi");
 </section>
 
 
-  <section class="footer">
-      <div class="footer-container">
-
-        <div class="footer-logo-section">
-          <div class="footer-logo">
-            <img src="./Source/<?php echo $f['logo']; ?>" alt="Ayam Goreng Monas Logo" />
-            <p class="footer-slogan"><?php echo $f['slogan']; ?></p>
-          </div>
-          <div class="footer-description">
-            <p>Ayam goreng terbaik dengan cita rasa autentik dan bumbu khas yang telah dipercaya oleh ribuan pelanggan setia kami.</p>
-          </div>
-        </div>
-
-        <div class="footer-column">
-          <h3>Link</h3>
-          <ul>
-            <li><a href="<?php echo $f['link_story']; ?>">Cerita</a></li>
-            <li><a href="<?php echo $f['link_menu']; ?>">Menu</a></li>
-            <li><a href="<?php echo $f['link_news']; ?>">Berita</a></li>
-          </ul>
-        </div>
-
-        <div class="Footer-cabang">
-          <h3>Alamat Kami</h3>
-          <div class="footer-cabang-content">
-            <p><?php echo $f['alamat']; ?></p>
-          </div>
-          <div class="footer-social">
-            <a href="<?php echo $f['instagram']; ?>" class="social-icon">
-              <img src="./source/instagram.svg" alt="Instagram" />
-            </a>
-            <a href="<?php echo $f['tiktok']; ?>" class="social-icon">
-              <img src="./source/tiktok.svg" alt="TikTok" />
-            </a>
-            <a href="<?php echo $f['x']; ?>" class="social-icon">
-              <img src="./source/x.svg" alt="X" />
-            </a>
-            <a href="<?php echo $f['facebook']; ?>" class="social-icon">
-              <img src="./source/facebook.svg" alt="Facebook" />
-            </a>
-          </div>
-        </div>
-
-      </div>
-
-      <div class="footer-bottom">
-        <p>Copyright &copy; 2025 Ayam Goreng Monas. Hak Cipta Dilindungi</p>
-      </div>
-    </section>
-
+<?php include 'footer.php'; ?>
 
 <script>
     feather.replace();
